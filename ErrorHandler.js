e2b1eae9da32f70db6d5b482f2fa75a20ab02380
Disura Randunu <disurarandunu@gmail.com>
2024-10-18 20:59:50 +0530

    Error handler class added hor centralized error handling

---
+++ ErrorHandler.js
@@ -0,0 +1,26 @@
+import { AppError } from "./AppError.js"
+
+export class ErrorHandler {
+    async handleError(error, response) {
+        console.error(error)
+        await this.sendErrorResponse(error, response)
+    }
+
+    async sendErrorResponse(error, response) {
+        if (error instanceof AppError) {
+            response.status(error.httpCode).json({
+                status: false,
+                message: error.message,
+                data: null
+            })
+        } else {
+            response.status(500).json({
+                status: false,
+                message: 'Something went wrong. Please contact administrator.',
+                data: null
+            })
+        }
+    }
+
+    // define more error methods such as sendLogs, alertAdmin and use them as required in handleError function.
+}
