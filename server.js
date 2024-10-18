71462c55f8e0bdd408e76f59bad794104635ccb4
Disura Randunu <disurarandunu@gmail.com>
2024-10-18 21:00:24 +0530

    Error handling middleware

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express')
+import express from 'express'
+import { ErrorHandler } from './ErrorHandler.js'
+
 const app = express()
 const port = 3000
+
+const errorHandler = new ErrorHandler
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.use(handleError)
-
+app.use(async (err, req, res, next) => {
+  await errorHandler.handleError(err, res)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
